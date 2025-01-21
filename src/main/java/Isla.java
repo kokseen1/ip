dd4f18adb89911ec2a2b942d6ec7a3b74731887f
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-01-21 14:40:03 +0800

    Implement Mark and Unmark features

--- src/main/java/Isla.java
+++ src/main/java/Isla.java
@@ -1,32 +1,56 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Isla {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] list = new String[100];
-        int listSize = 0;
+        ArrayList<Task> taskList = new ArrayList<>();
+        String answer;
 
         System.out.println("Hello, I am Isla.\nWhat can I do for you?");
-        String answer = input.nextLine();
+
         while (true) {
+            answer = input.nextLine();
+
             switch (answer) {
                 case "bye":
                     System.out.println("Bye. Hope to see you again.");
                     return;
 
                 case "list":
-                    for (int i = 0; i < listSize; i++) {
-                        System.out.println(i+1 + ". " + list[i]);
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        Task task = taskList.get(i);
+                        System.out.println(i + 1 + ". " + task.getFormattedString());
                     }
                     break;
 
                 default:
-                    list[listSize] = answer;
-                    listSize++;
-                    System.out.println("Added: " + answer);
-                    break;
+                    String[] splittedAnswer = answer.split(" ");
+                    String command = splittedAnswer[0];
+                    switch (command) {
+                        case "mark": {
+                            Task task = taskList.get(Integer.parseInt(splittedAnswer[1]) - 1);
+                            task.markAsDone();
+                            System.out.println("Nice! I've marked this task as done:");
+                            System.out.println(task.getFormattedString());
+                            break;
+                        }
+
+                        case "unmark": {
+                            Task task = taskList.get(Integer.parseInt(splittedAnswer[1]) - 1);
+                            task.markAsNotDone();
+                            System.out.println("OK, I've marked this task as not done yet:");
+                            System.out.println(task.getFormattedString());
+                            break;
+                        }
+
+                        default:
+                            taskList.add(new Task(answer));
+                            System.out.println("Added: " + answer);
+                            break;
+                    }
             }
-            answer = input.nextLine();
         }
     }
 }
