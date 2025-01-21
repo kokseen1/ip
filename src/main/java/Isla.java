5209ceea65c6491ae1ff6eeed0954550daeb708c
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-01-21 13:32:29 +0800

    Implement Rename, Greet, Exit features

---
+++ src/main/java/Isla.java
@@ -0,0 +1,6 @@
+public class Isla {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Isla.\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again.");
+    }
+}
