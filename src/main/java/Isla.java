68992bd6c5815694344517c242056ebdc32b9fe9
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-01-21 14:02:39 +0800

    Implement Add and List features

--- src/main/java/Isla.java
+++ src/main/java/Isla.java
@@ -3,12 +3,30 @@ import java.util.Scanner;
 public class Isla {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] list = new String[100];
+        int listSize = 0;
+
         System.out.println("Hello, I am Isla.\nWhat can I do for you?");
         String answer = input.nextLine();
-        while (!answer.equals("bye")) {
-            System.out.println(answer);
+        while (true) {
+            switch (answer) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again.");
+                    return;
+
+                case "list":
+                    for (int i = 0; i < listSize; i++) {
+                        System.out.println(i+1 + ". " + list[i]);
+                    }
+                    break;
+
+                default:
+                    list[listSize] = answer;
+                    listSize++;
+                    System.out.println("Added: " + answer);
+                    break;
+            }
             answer = input.nextLine();
         }
-        System.out.println("Bye. Hope to see you again.");
     }
 }
