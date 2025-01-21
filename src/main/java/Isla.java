b24adf73fd115865bfb65e27b72765fc7d061f91
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2025-01-21 13:48:07 +0800

    Implement Echo feature

--- src/main/java/Isla.java
+++ src/main/java/Isla.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Isla {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello, I am Isla.\nWhat can I do for you?");
+        String answer = input.nextLine();
+        while (!answer.equals("bye")) {
+            System.out.println(answer);
+            answer = input.nextLine();
+        }
         System.out.println("Bye. Hope to see you again.");
     }
 }
